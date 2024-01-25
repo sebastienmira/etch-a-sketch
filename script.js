e99648abe22a490a0602cb2429b8df0ff24108f6
sebastienmira <sebastienm.2001@gmail.com>
2024-01-25 15:59:32 +0200

    grid created

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let gridSize=16
+let borderWidth=5
 
 const container= document.querySelector('#container')
-const squareSize=container.offsetHeight/gridSize
+container.setAttribute('style', 'border-width:5px')
+
+const squareSize=(container.offsetHeight-2*borderWidth)/(gridSize)
 
 for(let i=0; i<gridSize**2; i++){
   const square= document.createElement('div')
   square.classList.add('square')
-  square.setAttribute('style', `width: ${squareSize}px; heigth:${squareSize}px`)
+  square.setAttribute('style', `width: ${squareSize}px; height:${squareSize}px`)
   container.appendChild(square)
 }

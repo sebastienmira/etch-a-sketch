898989ed6e54abed5cd485b49a42e0644cfc1814
sebastienmira <sebastienm.2001@gmail.com>
2024-01-26 14:12:05 +0200

    change grid size and b&w

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-let gridSize=16
-let borderWidth=5
-
 const container= document.querySelector('#container')
 container.setAttribute('style', 'border-width:5px')
 
-const squareSize=(container.offsetHeight-2*borderWidth)/(gridSize)
+let defaultGrid=generateGrid(16)
 
-for(let i=0; i<gridSize**2; i++){
-  const square= document.createElement('div')
-  square.classList.add('square')
-  square.setAttribute('style', `width: ${squareSize}px; height:${squareSize}px`)
-  container.appendChild(square)
+
+function generateGrid(gridSize){
+  let borderWidth=5
+  let squareSize=(container.offsetHeight-2*borderWidth)/(gridSize)
+  
+  for(let i=0; i<gridSize**2; i++){
+    const square= document.createElement('div')
+    square.classList.add('square')
+    square.setAttribute('style', `width: ${squareSize}px; height:${squareSize}px`)
+    container.appendChild(square)
+    square.addEventListener('mouseover', ()=>{
+      square.classList.toggle('blackSquare')
+    })
+  }
 }
+
+const gridSelector= document.querySelector('#gridSelector')
+
+
+gridSelector.addEventListener('click', ()=>{
+  document.querySelectorAll('.square').forEach((square)=>{
+    container.removeChild(square)
+  })
+  generateGrid(Number(prompt('Enter grid width:')))
+})
+
+
+defaultGrid
